6932030d4f6bc96df9b85d9d1986cf1e1ce198eb
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-04-15 21:50:08 -0500

    Create validation.js
    Checks file type using multer.

--- Client/Input Validation/validation.js
+++ Client/Input Validation/validation.js
@@ -2,62 +2,41 @@ const express = require('express');
 const multer = require('multer');
 const sqlite3 = require('sqlite3').verbose();
 const path = require('path');
-
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = 3000;
 
-const conn = new sqlite3.Database('media.db');
+// database connection
+const dbPath = path.resolve(__dirname, 'media.db');
+const db = new sqlite3.Database(dbPath);
 
+// uploading files
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, 'static/uploads/');
+  destination: (req, file, cb) => {
+    cb(null, path.resolve(__dirname, 'static/uploads'));
   },
-  filename: function (req, file, cb) {
-    cb(null, Date.now() + path.extname(file.originalname));
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now());
   }
 });
 
-const upload = multer({ //middleware
-  storage: storage,
-  limits: { fileSize: 1000000 }, // file size limit of 1 MB
-  fileFilter: function (req, file, cb) {
-    checkFileType(file, cb);
-  }
-}).single('mediaFile');
+// multer middleware forfile uploads
+const upload = multer({ storage: storage });
 
-
-function checkFileType(file, cb) {
-  const filetypes = /jpeg|jpg|png|gif/;
-  const extname = filetypes.test(path.extname(file.originalname).toLowerCase());
-  const mimetype = filetypes.test(file.mimetype);
-  if (extname && mimetype) {
-    return cb(null, true);
-  } else {
-    return cb('Error: Images and GIFs only');
-  }
-}
-
-// file uploads
-app.post('/upload', function (req, res) {
-  upload(req, res, function (err) {
+app.post('/upload', upload.single('file'), (req, res) => {
+  // file metadata goes to SQL database
+  const { originalname, mimetype, filename, size } = req.file;
+  const query = `INSERT INTO media (filename, mimetype, size) VALUES (?, ?, ?)`;
+  db.run(query, [filename, mimetype, size], (err) => {
     if (err) {
-      res.status(400).json({ message: err });
-    } else {
-      const { filename, mimetype, size } = req.file;
-      const { userId } = req.body;
-      // file metadata goes to SQL database
-      conn.run('INSERT INTO media (filename, mimetype, size, user_id) VALUES (?, ?, ?, ?)', [filename, mimetype, size, userId], function (err) {
-        if (err) {
-          res.status(500).json({ message: 'Error saving file metadata to database' });
-        } else {
-          res.status(200).json({ message: 'File uploaded successfully' });
-        }
-      });
+      console.error(err.message);
+      return res.status(500).json({ message: 'Error storing file metadata' });
     }
+    // url goes to client
+    const fileUrl = req.protocol + '://' + req.get('host') + '/uploads/' + filename;
+    res.status(200).json({ url: fileUrl });
   });
 });
 
-// start server
-app.listen(PORT, function () {
-  console.log(`Server started on port ${PORT}`);
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
 });
