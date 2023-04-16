158412b232587f7ac8632c78d602c0028c600369
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-04-16 15:49:09 -0500

    Update editing.jsx

--- Client/HTML templates/editing.jsx
+++ Client/HTML templates/editing.jsx
@@ -1,50 +1,28 @@
-import React, { useState, useRef } from 'react';
-import Cropper from 'react-easy-crop';
-import { Slider } from '@material-ui/core';
-import { getCroppedImg, getRotatedImage, applyFilter } from './imageUtils';
+import React, { useState } from 'react';
 
-const EditImage = ({ imageUrl }) => {
-  const [crop, setCrop] = useState({ x: 0, y: 0 });
-  const [rotation, setRotation] = useState(0);
-  const [zoom, setZoom] = useState(1);
-  const [filter, setFilter] = useState('');
+function EditingPage(props) {
+  const [imageSrc, setImageSrc] = useState('');
 
-  const imageRef = useRef(null);
-  const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
-
-  const onCropComplete = (croppedArea, croppedAreaPixels) => {
-    setCroppedAreaPixels(croppedAreaPixels);
+  const handleFileInputChange = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = (event) => {
+      setImageSrc(event.target.result);
+    };
   };
-
-  const handleCrop = async () => {
-    const croppedImage = await getCroppedImg(imageUrl, croppedAreaPixels);
-    // do stuff with cropped image
-  };
-
-  const handleRotate = () => {
-    setRotation(rotation + 90);
-  };
-
-  const handleZoom = (event, value) => {
-    setZoom(value);
-  };
-
-  const handleFilterChange = (event) => {
-    setFilter(event.target.value);
-  };
-
-  const filteredImage = applyFilter(imageUrl, filter);
 
   return (
     <div>
-      <div style={{ display: 'flex' }}>
-        <div style={{ width: '50%', marginRight: '1rem' }}>
-          <Cropper
-            image={filteredImage}
-            crop={crop}
-            rotation={rotation}
-            zoom={zoom}
-            aspect={1}
-            onCropChange={setCrop}
-            onRotationChange={setRotation}
-            onCropComplete={onCropComplete}
+      <h1>Editing Page</h1>
+      <input type="file" onChange={handleFileInputChange} />
+      {imageSrc && (
+        <img src={imageSrc} alt="Uploaded media" />
+      )}
+      {/* code for image editing features here */}
+      <button onClick={() => props.onSubmit(imageSrc)}>Share</button>
+    </div>
+  );
+}
+
+export default EditingPage;
