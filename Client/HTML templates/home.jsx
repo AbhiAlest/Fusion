261d28a1b9a0d827ce57338de1837e184f177114
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-04-17 17:12:39 -0500

    home.jsx
    Now with dark mode, menu, and a dividing bar.

---
+++ Client/HTML templates/home.jsx
@@ -0,0 +1,39 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+
+function Home() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+  const toggleDarkMode = () => setIsDarkMode(!isDarkMode);
+
+  return (
+    <div className={`container ${isDarkMode ? 'dark-mode' : ''}`}>
+      <div className="navbar">
+        <div className="logo">
+          <a href="https://example.com">
+            <img src="/images/logo.png" alt="Fusion logo" />
+          </a>
+        </div>
+        <div className="menu">
+          <ul>
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/upload">Upload</Link></li>
+            <li><Link to="/gallery">Gallery</Link></li>
+          </ul>
+        </div>
+        <div className="toggle" onClick={toggleDarkMode}>
+          {isDarkMode ? (
+            <i className="fas fa-sun"></i>
+          ) : (
+            <i className="fas fa-moon"></i>
+          )}
+        </div>
+      </div>
+      <div className="divider"></div>
+      <div className="content">
+        {/* Add your existing content here */}
+      </div>
+    </div>
+  );
+}
+
+export default Home;
