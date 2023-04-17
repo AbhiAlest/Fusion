d1e1c3950201d0563f06ecef9180e02baa56049c
AbhiAlest <128109884+AbhiAlest@users.noreply.github.com>
2023-04-17 17:28:23 -0500

    Update home.jsx

--- Client/HTML templates/home.jsx
+++ Client/HTML templates/home.jsx
@@ -1,5 +1,7 @@
+import './Home.css';
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
+
 
 function Home() {
   const [isDarkMode, setIsDarkMode] = useState(false);
